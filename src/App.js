09f567fa13c1327f355420974c523da636866d7d
nirinaRakotoniary <nirinamercia@gmail.com>
2022-06-29 19:00:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
+function App() {
+return(
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+}
+
+export default App;
